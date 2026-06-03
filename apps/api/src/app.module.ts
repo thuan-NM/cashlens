8c4d675a591240daa64f240fa4244b99b12f2d00
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2026-06-03 23:38:19 +0700

    feat: restructure API module by removing unused components and adding Users module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UsersModule],
 })
 export class AppModule {}
